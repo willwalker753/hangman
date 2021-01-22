0a8edd0382363a0238f2385f8eb9737a7320a580
Will Walker <williamwalker753@gmail.com>
2021-01-21 19:39:16 -0600

    init commit

---
+++ src/components/hangman.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class hangman extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <p>hey</p>
+            </div>
+        )
+    }
+}
