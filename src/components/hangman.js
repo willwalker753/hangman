a15ef5c84210a7149afebbbf8007621ad05a5d3e
Will Walker <williamwalker753@gmail.com>
2021-01-22 13:18:02 -0600

    cycle images

--- src/components/hangman.js
+++ src/components/hangman.js
@@ -5,35 +5,51 @@ export default class hangman extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            hangmanImage: '.\images\hangman0.png',
+            hangmanImage: 'https://github.com/willwalker753/hangman/blob/main/src/images/hangman0.png?raw=true',
+            imageNum: 0,
             guessed: '',
             guessedArr: [],
             answer: '',
             answerArr: ['_','_','_','_','_','_'],
         }
     }
+    cycleImage = e => {
+        let num = this.state.imageNum + 1;
+        let image = 'https://github.com/willwalker753/hangman/blob/main/src/images/hangman'+ num +'.png?raw=true';
+        this.setState({
+            hangmanImage: image,
+            imageNum: num,
+        });
+    }
     pressKey = e => {
         let letter = e.target.value.slice(0,1);
+        document.getElementById('letter-input').reset();
         if(this.state.guessedArr.length < 5) {
             this.state.guessedArr.push(letter);
-            this.setState({ guessed:  this.state.guessed + ' ' + letter });      
+            this.setState({ guessed:  this.state.guessed + ' ' + letter }); 
+            this.cycleImage();     
         }
         else if(this.state.guessedArr.length === 5) {
             let answer = brain.findNoMatch(this.state.guessedArr);
             this.setState({ answer: answer });
+            this.setState({ guessed:  this.state.guessed + ' ' + letter });
             this.state.guessedArr.push(letter);
+            this.cycleImage(); 
         }
-        else if(this.state.guessedArr.length >= 5) { 
+        else if(this.state.guessedArr.length > 5) { 
+            console.log(letter)
             this.state.guessedArr.push(letter);
             this.setState({ guessed:  this.state.guessed + ' ' + letter });
             let answerArr = brain.checkGuess(this.state.guessedArr, this.state.answer, this.state.answerArr);
             this.setState({ answerArr: answerArr })
         }
-        document.getElementById('letter-input').reset();
+        
     }
     reset = e => {
         e.preventDefault();
         this.setState({
+            hangmanImage: 'https://github.com/willwalker753/hangman/blob/main/src/images/hangman0.png?raw=true',
+            imageNum: 0,
             guessed: '',
             guessedArr: [],
             answer: '',
